f558d2b4b9df91df1bfbd896b31123f9d9b305d6
szchan <mail.szchan@gmail.com>
2025-12-08 17:28:44 +0800

    Add disko pre-configuration and run script for disk setup

---
+++ disko-pre.md
@@ -0,0 +1,4 @@
+lsblk
+
+Replace <disk-name> with the name of your disk obtained in Step 1.
+将<disk-name> 替换 为第一步中获得的磁盘名称。
---
+++ disko-run.sh
@@ -0,0 +1,1 @@
+sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount /tmp/disk-config.nix
