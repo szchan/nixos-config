f1ae97fec133e239a0ae71ebbdf97352d5b042e5
szchan <mail.szchan@gmail.com>
2025-12-09 16:17:10 +0800

    Add disko configuration and update flake.nix to include disko and vscode-server modules

--- flake.nix
+++ flake.nix
@@ -2,28 +2,38 @@
   description = "gateway flake";
 
   inputs = {
-    # NixOS 官方软件源，这里使用 nixos-25.11 分支
+    # NixOS 官方软件源，这里使用 nixos-unstable 分支
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     # disko 模块
-    disko.url = "github:nix-community/disko";
-    disko.inputs.nixpkgs.follows = "nixpkgs";
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     # vscode-server 模块
-    vscode-server.url = "github:nix-community/nixos-vscode-server";
+    vscode-server = {
+      url = "github:nix-community/nixos-vscode-server";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, vscode-server, ... }@inputs: {
     # 定义 NixOS 系统配置
     nixosConfigurations.szchanNixOSStation = nixpkgs.lib.nixosSystem {
       modules = [
-        # 这里导入 configuration.nix，
-        # 这样旧的配置文件仍然能生效
         ./configuration.nix
-        # 导入 vscode-server 模块
-        ./develop/vscode-remote-server.nix
         # 导入 disko 模块
-        ./disko/disko.nix
+        disko.nixosModules.disko
+        {
+          disko = import ./disko/disko.nix;
+        }
+        # 导入 vscode-server 模块
+        vscode-server.nixosModules.default
+        {
+          vscode-server = import ./develop/vscode-remote-server.nix;
+        }
       ];
     };
   };
