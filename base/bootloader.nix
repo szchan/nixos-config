03750d101be8ae1f5508fdcfcaf55328fefcac0c
szchan <mail.szchan@gmail.com>
2025-12-14 23:56:48 +0800

    Fix syntax for systemd-boot configuration in bootloader.nix

--- base/bootloader.nix
+++ base/bootloader.nix
@@ -1,7 +1,7 @@
-{config, pkgs, ... }:
+{config, pkgs, lib, ... }:
 
 {
-    boot.loader.systemd-boot {
+    boot.loader.systemd-boot = {
         enable = true;
 
         # Limit the number of boot entries to keep
@@ -10,7 +10,7 @@
         # pick the highest resolution for systemd-boot's console.
         consoleMode = lib.mkDefault "max";
 
-    }
+    };
 
     # wait for x seconds to select the boot entry.
     boot.loader.timeout = lib.mkDefault 8; 
