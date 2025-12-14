a03f7a289e95dd1204d5fd9c8be3088a1d7fddd1
szchan <mail.szchan@gmail.com>
2025-12-14 17:02:30 +0800

    Add bootloader and kernel configurations for NixOS setup

--- configuration.nix
+++ configuration.nix
@@ -21,11 +21,11 @@
   services.vscode-server.enable = true;
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.loader.systemd-boot.enable = true;
+  # boot.loader.efi.canTouchEfiVariables = true;
 
   # Use latest kernel.
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  # boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "szchanNixOSStation"; # Define your hostname.
 
