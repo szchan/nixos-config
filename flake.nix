00b2c9569bfed300dd83eb5fae1000229d946b1b
szchan <mail.szchan@gmail.com>
2025-12-20 22:23:25 +0800

    Update home-manager user configuration and enable zsh with completion

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
         home-manager.nixosModules.home-manager{
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.jdoe = ./home.nix;
+          home-manager.users.szchan = ./home.nix;
 
           # Optionally, use home-manager.extraSpecialArgs to pass
           # arguments to home.nix
