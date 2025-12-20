d8c68b13c667100e49efca48a1899a86fcd53622
szchan <mail.szchan@gmail.com>
2025-12-20 18:30:56 +0800

    Comment out zsh configuration and related aliases in home.nix

--- home.nix
+++ home.nix
@@ -102,22 +102,22 @@
     };
     
     # 配置 zsh
-    programs.zsh = {
-        enable = true;
-        enableCompletion = true;
-        # TODO 在这里添加你的自定义 zshrc 内容
-        zshrcExtra = ''
+    # programs.zsh = {
+    #     enable = true;
+    #     enableCompletion = true;
+    #     # TODO 在这里添加你的自定义 zshrc 内容
+    #     zshrcExtra = ''
 
-        '';
+    #     '';
 
-        # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
-        shellAliases = {
-        #   k = "kubectl";
-        #   urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-        #   urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-        };
-    };
-    
+    #     # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
+    #     shellAliases = {
+    #     #   k = "kubectl";
+    #     #   urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+    #     #   urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+    #     };
+    # };
+
     # This value determines the Home Manager release that your
     # configuration is compatible with. This helps avoid breakage
     # when a new Home Manager release introduces backwards
