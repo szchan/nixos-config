1be24d177ec093f4c17f0963dfc27cc161ba6b33
szchan <mail.szchan@gmail.com>
2025-12-23 15:17:20 +0800

    fix syntax error

--- home/szchan/terminal/zsh.nix
+++ home/szchan/terminal/zsh.nix
@@ -6,19 +6,21 @@
         enableCompletion = true;
         syntaxHighlighting.enable = true;
         autosuggestion.enable = true;
-        # TODO 在这里添加你的自定义 zshrc 内容
+        
         localVariables = {
-            EDITOR=nvim;
-            PATH=$PATH:~/.local/bin/;
+            
         };
 
-        # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
+        # 设置一些别名方便使用，你可以根据自己的需要进行增删
         shellAliases = {
             
         };
 
+        # 在这里添加自定义 zshrc 内容
         initContent = "
-            
+            fastfetch
+            export PATH=$PATH:~/.local/bin/
+            export EDITOR=nvim
         "
     };
 
