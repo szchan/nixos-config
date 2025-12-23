5cf483c14c39525ad788c0c37d41315c17004844
szchan <mail.szchan@gmail.com>
2025-12-23 15:13:52 +0800

    update zsh config

--- home/szchan/terminal/zsh.nix
+++ home/szchan/terminal/zsh.nix
@@ -8,13 +8,18 @@
         autosuggestion.enable = true;
         # TODO 在这里添加你的自定义 zshrc 内容
         localVariables = {
-            
+            EDITOR=nvim;
+            PATH=$PATH:~/.local/bin/
         };
 
         # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
         shellAliases = {
             
         };
+
+        initContent = "
+            fastfetch
+        "
     };
 
 }
