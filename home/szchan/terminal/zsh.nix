a33fe4dcb5b3a3a7cf7e040bf14e250a537fec43
szchan <mail.szchan@gmail.com>
2025-12-23 14:10:14 +0800

    change to homemanager config options

--- home/szchan/terminal/zsh.nix
+++ home/szchan/terminal/zsh.nix
@@ -5,17 +5,12 @@
         enable = true;
         enableCompletion = true;
         syntaxHighlighting.enable = true;
-        autosuggestions.enable = true;
+        autosuggestion.enable = true;
         # TODO 在这里添加你的自定义 zshrc 内容
-        shellInit = ''
-
+        localVariables = ''
+        
         '';
-
-        # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
-        shellAliases = {
-        #   k = "kubectl";
-        #   urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-        #   urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-        };
     };
+
+    programs.zoxide.enableZshIntegration = true;
 }
