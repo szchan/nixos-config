d7a274883c365433558a5c806c880a0891dbbb36
szchan <mail.szchan@gmail.com>
2025-12-23 15:56:45 +0800

    fix syntax

--- home/szchan/terminal/fastfetch.nix
+++ home/szchan/terminal/fastfetch.nix
@@ -4,92 +4,92 @@
     programs.fastfetch = {
         enable = true;
         settings = {
-            "logo": {
-                "type": "small",
-                "padding": {
-                    "top": 1
+            "logo" = {
+                "type" = "small";
+                "padding" = {
+                    "top" = 1;
+                };
+            };
+            "display" = {
+                "separator" = " ";
+            };
+            "modules" = [
+                {
+                    key = "╭───────────╮";
+                    type = "custom";
                 }
-            },
-            "display": {
-                "separator": " "
-            },
-            "modules": [
                 {
-                    "key": "╭───────────╮",
-                    "type": "custom"
-                },
+                    key = "│ {#31} user    {#keys}│";
+                    type = "title";
+                    format = "{user-name}";
+                }
                 {
-                    "key": "│ {#31} user    {#keys}│",
-                    "type": "title",
-                    "format": "{user-name}"
-                },
+                    key = "│ {#32}󰇅 hname   {#keys}│";
+                    type = "title";
+                    format = "{host-name}";
+                }
                 {
-                    "key": "│ {#32}󰇅 hname   {#keys}│",
-                    "type": "title",
-                    "format": "{host-name}"
-                },
+                    key = "│ {#33}󰅐 uptime  {#keys}│";
+                    type = "uptime";
+                }
                 {
-                    "key": "│ {#33}󰅐 uptime  {#keys}│",
-                    "type": "uptime"
-                },
+                    key = "│ {#34}{icon} distro  {#keys}│";
+                    type = "os";
+                }
                 {
-                    "key": "│ {#34}{icon} distro  {#keys}│",
-                    "type": "os"
-                },
+                    key = "│ {#35} kernel  {#keys}│";
+                    type = "kernel";
+                }
                 {
-                    "key": "│ {#35} kernel  {#keys}│",
-                    "type": "kernel"
-                },
+                    key = "│ {#36} wm      {#keys}│";
+                    type = "wm";
+                }
                 {
-                    "key": "│ {#36} wm      {#keys}│",
-                    "type": "wm"
-                },
+                    key = "│ {#36}󰇄 desktop {#keys}│";
+                    type = "de";
+                }
                 {
-                    "key": "│ {#36}󰇄 desktop {#keys}│",
-                    "type": "de"
-                },
+                    key = "│ {#31} term    {#keys}│";
+                    type = "terminal";
+                }
                 {
-                    "key": "│ {#31} term    {#keys}│",
-                    "type": "terminal"
-                },
+                    key = "│ {#32} shell   {#keys}│";
+                    type = "shell";
+                }
                 {
-                    "key": "│ {#32} shell   {#keys}│",
-                    "type": "shell"
-                },
+                    key = "│ {#33}󰍛 cpu     {#keys}│";
+                    type = "cpu";
+                    showPeCoreCount = true;
+                }
                 {
-                    "key": "│ {#33}󰍛 cpu     {#keys}│",
-                    "type": "cpu",
-                    "showPeCoreCount": true
-                },
+                    key = "│ {#34}󰍜 gpu     {#keys}│";
+                    type = "gpu";
+                }
                 {
-                    "key": "│ {#34}󰍜 gpu     {#keys}│",
-                    "type": "gpu"
-                },
+                    key = "│ {#36} memory  {#keys}│";
+                    type = "memory";
+                }
                 {
-                    "key": "│ {#36} memory  {#keys}│",
-                    "type": "memory"
-                },
+                    key = "│ {#37} swap    {#keys}│";
+                    type = "swap";
+                }
                 {
-                    "key": "│ {#37} swap    {#keys}│",
-                    "type": "swap"
-                },
+                    key = "│ {#38}󰉉 disk    {#keys}│";
+                    type = "disk";
+                    folders = "/";
+                }
                 {
-                    "key": "│ {#38}󰉉 disk    {#keys}│",
-                    "type": "disk",
-                    "folders": "/"
-                },
+                    key = "├───────────┤";
+                    type = "custom";
+                }
                 {
-                    "key": "├───────────┤",
-                    "type": "custom"
-                },
+                    key = "│ {#39} colors  {#keys}│";
+                    type = "colors";
+                    symbol = "circle";
+                }
                 {
-                    "key": "│ {#39} colors  {#keys}│",
-                    "type": "colors",
-                    "symbol": "circle"
-                },
-                {
-                    "key": "╰───────────╯",
-                    "type": "custom"
+                    key = "╰───────────╯";
+                    type = "custom";
                 }
             ]
         }
