5de4f01fb625b3739ea8e9a6e6c2c2436894403a
szchan <mail.szchan@gmail.com>
2025-12-24 00:05:36 +0800

    update niri config

--- home/szchan/desktopEnvironment/niri/niri.nix
+++ home/szchan/desktopEnvironment/niri/niri.nix
@@ -1,7 +1,18 @@
 { config, pkgs, ... }:
 
 {
-    # Enable Niri session
-    # xsession.windowManager.niri.enable = true;
-    nixpkgs.overlays = [ niri.overlays.niri ];
+    # 添加 overlay，让 niri-unstable 等包可用
+    nixpkgs.overlays = [ pkgs.niri-flake.overlays.niri ];
+
+    imports = [
+        pkgs.niri-flake.homeManagerModules.niri  # 导入 niri 的官方 home-manager 模块
+    ];
+
+    programs.niri = {
+    enable = true;
+
+    # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
+    package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
+
+    }
 }
