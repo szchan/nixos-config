fdea23c6863c5eb7640f015bfc138abaeafac672
szchan <mail.szchan@gmail.com>
2025-12-23 17:07:54 +0800

    update starship config

--- home/szchan/terminal/starship.nix
+++ home/szchan/terminal/starship.nix
@@ -5,10 +5,7 @@
         enable = true;
         # 自定义配置
         settings = {
-            add_newline = false;
-            aws.disabled = true;
-            gcloud.disabled = true;
-            line_break.disabled = true;
+            add_newline = true;
         };
     };
 }
