42b3c659502de70a5ec0bad017561c24a5d24150
szchan <mail.szchan@gmail.com>
2025-12-23 14:37:43 +0800

    fix alias config of zsh

--- home/szchan/terminal/zsh.nix
+++ home/szchan/terminal/zsh.nix
@@ -8,7 +8,12 @@
         autosuggestion.enable = true;
         # TODO 在这里添加你的自定义 zshrc 内容
         localVariables = {
-            alias z = "zoxide";
+            
+        };
+
+        # TODO 设置一些别名方便使用，你可以根据自己的需要进行增删
+        shellAliases = {
+            z = "zoxide";
         };
     };
 
