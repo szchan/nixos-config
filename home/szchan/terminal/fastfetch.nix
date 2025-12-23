065294994642b55c250a739f53a484dae75c483a
szchan <mail.szchan@gmail.com>
2025-12-23 15:51:22 +0800

    remove $schema

--- home/szchan/terminal/fastfetch.nix
+++ home/szchan/terminal/fastfetch.nix
@@ -4,7 +4,6 @@
     programs.fastfetch = {
         enable = true;
         settings = {
-            "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
             "logo": {
                 "type": "small",
                 "padding": {
