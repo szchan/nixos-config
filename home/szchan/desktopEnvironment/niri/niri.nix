619bf7a59b315d15cf2663c2f3ef3949264243b2
szchan <mail.szchan@gmail.com>
2025-12-24 09:30:55 +0800

    unset unstable niri

--- home/szchan/desktopEnvironment/niri/niri.nix
+++ home/szchan/desktopEnvironment/niri/niri.nix
@@ -6,7 +6,7 @@
         enable = true;
 
         # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
-        package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
+        # package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
 
     };
 }
