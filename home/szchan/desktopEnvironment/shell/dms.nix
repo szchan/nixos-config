b98de1d047a4b9dfcc7e953f8e9c7352067567bc
szchan <mail.szchan@gmail.com>
2025-12-24 09:33:42 +0800

    unuse dms niri binds

--- home/szchan/desktopEnvironment/shell/dms.nix
+++ home/szchan/desktopEnvironment/shell/dms.nix
@@ -4,10 +4,10 @@
 
     programs.dankMaterialShell = {
         enable = true;
-        niri = {
-            enableKeybinds = true;   # Sets static preset keybinds
-            enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
-        };
+        # niri = {
+        #     enableKeybinds = true;   # Sets static preset keybinds
+        #     enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
+        # };
 
         enableSystemMonitoring = true;     # System monitoring widgets (dgop)
         enableClipboard = true;            # Clipboard history manager
