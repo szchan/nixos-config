494421347862c03230d3ad7e10035b1d7b76f7aa
szchan <mail.szchan@gmail.com>
2025-12-25 13:59:34 +0800

    check niri

--- desktop/niri/niri.nix
+++ desktop/niri/niri.nix
@@ -9,9 +9,9 @@
     environment.systemPackages = with pkgs; [
         alacritty  # 终端
         fuzzel     # 应用启动器（类似 rofi）
-        # waybar     # 状态栏（可选）
-        # swayidle   # 闲置管理
-        # swaylock   # 锁屏
+        waybar     # 状态栏（可选）
+        swayidle   # 闲置管理
+        swaylock   # 锁屏
         xdg-desktop-portal-gnome  # 屏幕共享等 portal 支持
         dms-shell
     ];
@@ -24,34 +24,34 @@
     };
 
     # display manager
-    services.displayManager.dms-greeter = {
-        enable = true;
-        # package = pkgs.dms-greeter;
-        quickshell.package = pkgs.quickshell;
-        logs = {
-            save = true;
-            path = "/var/log/dms-greeter.log";
-        };
-        compositor.name = "niri";
+    # services.displayManager.dms-greeter = {
+    #     enable = true;
+    #     # package = pkgs.dms-greeter;
+    #     quickshell.package = pkgs.quickshell;
+    #     logs = {
+    #         save = true;
+    #         path = "/var/log/dms-greeter.log";
+    #     };
+    #     compositor.name = "niri";
 
-    };
+    # };
 
     # Desktop Shell
-    programs.dms-shell = {
-        enable = true;
-        package = pkgs.dms-shell;
-        systemd = {
-            # enable = true;
-            target = "graphical-session.target";
-            restartIfChanged = true;
-        };
-        quickshell.package = pkgs.quickshell;
-        enableVPN = true;
-        enableSystemMonitoring = true;
-        enableDynamicTheming = true;
-        enableClipboard = true;
-        enableCalendarEvents = true;
-        enableAudioWavelength = true;
-    };
+    # programs.dms-shell = {
+    #     enable = true;
+    #     package = pkgs.dms-shell;
+    #     systemd = {
+    #         # enable = true;
+    #         target = "graphical-session.target";
+    #         restartIfChanged = true;
+    #     };
+    #     quickshell.package = pkgs.quickshell;
+    #     enableVPN = true;
+    #     enableSystemMonitoring = true;
+    #     enableDynamicTheming = true;
+    #     enableClipboard = true;
+    #     enableCalendarEvents = true;
+    #     enableAudioWavelength = true;
+    # };
 
 }
