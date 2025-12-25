2296e7a757edfd9e9b906d910350619efb4b3aa9
szchan <mail.szchan@gmail.com>
2025-12-25 16:23:42 +0800

    add bluetooth support

--- base/base.nix
+++ base/base.nix
@@ -4,6 +4,7 @@
     imports = [ ./bootloader.nix
                 ./kernel.nix 
                 ./networking.nix
+                ./bluetooth.nix
                 ./i18n.nix
                 ./fonts.nix
                 ./qemuGuest.nix
