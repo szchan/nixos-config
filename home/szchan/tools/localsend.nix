c53b91dee308fc99fab2019a1deb515ffd177218
szchan <mail.szchan@gmail.com>
2025-12-25 17:39:52 +0800

    add localsend

---
+++ home/szchan/tools/localsend.nix
@@ -0,0 +1,12 @@
+{ config, pkgs, ... }:
+
+{
+    home.packages = with pkgs; [
+        localsend
+    ];
+
+    programs.localsend = {
+        enable = true;
+        openFirewall = true;
+    }
+}
