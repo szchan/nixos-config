ba9502f208a3334af3e627a472c432c3bbd8b05b
szchan <mail.szchan@gmail.com>
2025-12-26 21:18:59 +0800

    add Page Assist

--- home/szchan/internet/browser.nix
+++ home/szchan/internet/browser.nix
@@ -10,6 +10,7 @@
       { id = "fjkmabmdepjfammlpliljpnbhleegehm"; } # WebRTC Control
       { id = "bdiifdefkgmcblbcghdlonllpjhhjgof"; } # KISS Translator
       { id = "kefjpfngnndepjbopdmoebkipbgkggaa"; } # RSSHub Radar
+      { id = "jfgfiigpkhlkbnfnbobbkinehhfdhndo"; } # Page Assist
     ];
   };
 }
