0925d7510c5765c5aec9ea587ad37df502d7eb01
szchan <mail.szchan@gmail.com>
2025-12-27 20:10:09 +0800

    enable xdg.portal

--- desktop/gnome/gnome.nix
+++ desktop/gnome/gnome.nix
@@ -3,5 +3,7 @@
     # Enable the GNOME Desktop Environment.
     services.displayManager.gdm.enable = true;
     services.desktopManager.gnome.enable = true;
+
+    xdg.portal.enable = true;
     
 }
