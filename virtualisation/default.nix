577b25aa511a23f9b1e7ac905d79bd1ab883510d
szchan <mail.szchan@gmail.com>
2025-12-28 21:35:32 +0800

    enable podman

--- virtualisation/default.nix
+++ virtualisation/default.nix
@@ -4,5 +4,6 @@
   imports = [
     ./qemu-kvm.nix
     ./docker.nix
+    ./podman.nix
   ];
 }
---
+++ virtualisation/podman.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  virtualisation.containers.enable = true;
+
+  virtualisation.podman = {
+    enable = true;
+
+    # 于docker兼容模式，启用后Podman会创建一个与Docker兼容的socket
+    dockerCompat = false;
+
+    # docker-compose 指向 Podman socket
+    dockerSocket.enable = false;
+
+    # 启用 NAT 虚拟网络，用于podman-compose
+    defaultNetwork.settings.dns_enabled = true;
+
+  };
+
+  # hardware.nvidia-container-toolkit.enable = true;  # 如果需要 NVIDIA GPU 支持则启用此选项
+
+  environment.systemPackages = with pkgs; [
+    podman-compose
+  ];
+
+}
