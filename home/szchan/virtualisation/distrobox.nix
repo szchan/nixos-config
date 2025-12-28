36eefccd8a8117e2f25e5d9310dabb5d44059ac5
szchan <mail.szchan@gmail.com>
2025-12-28 21:45:48 +0800

    enable distrobox

---
+++ home/szchan/virtualisation/distrobox.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  programs.distrobox = {
+    enable = true;
+
+    
+    settings = {
+      container_manager = "podman";
+      container_user_custom_home = "${config.home.homeDirectory}/Data/distrobox";
+    };
+  };
+
+}
