bdea2b6b525fd963ef9cbab640f00c38ec44bf79
szchan <mail.szchan@gmail.com>
2025-12-28 18:15:20 +0800

    fix syntax

--- virtualisation/qemu-kvm.nix
+++ virtualisation/qemu-kvm.nix
@@ -1,15 +1,18 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: 
+
+{
+  boot.kernelModules = [ "kvm-intel" "vfio-pci" ]; # 如果使用 AMD CPU，则改为 "kvm-amd"
+
   virtualisation = {
     libvirtd = {
       enable = true;
       qemu = {
         swtpm.enable = true; # 启用软件 TPM 支持
-        ovmf.enable = true;  # 启用 UEFI 固件支持
-        runAsRoot = false;   # 出于安全考虑，不以 root 身份运行 QEMU
+        runAsRoot = true;    # 允许 QEMU 以 root 身份运行，以便访问某些硬件功能
       };
 
-      # 启用默认 NAT 虚拟网络
-      onBoot = "start";  # 确保 libvirtd 在 boot 时启动网络
+      onBoot = "ignore";  # 启动时不自动启动虚拟机,设置成"start"则自动启动
+      
     };
   };
 
@@ -19,10 +22,9 @@
     qemu          # QEMU 虚拟化工具
     qemu_kvm      # KVM 支持的 QEMU 版本
     libvirt       # libvirt 虚拟化管理工具
+    ovmf          # UEFI 固件支持
     swtpm         # 软件 TPM 支持
     dnsmasq       # 用于默认网络的 DHCP 服务
   ];
-
-  boot.kernelModules = [ "kvm-intel" ]; # 如果使用 AMD CPU，则改为 "kvm-amd"
 
 }
