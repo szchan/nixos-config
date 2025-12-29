a170b579c705f5c85aa494a2b23e5c958df2241e
szchan <mail.szchan@gmail.com>
2025-12-29 15:53:04 +0800

    fix syntax

--- home/szchan/tools/fcitx5/custom-rime.nix
+++ home/szchan/tools/fcitx5/custom-rime.nix
@@ -32,7 +32,7 @@ in
     # 下载并部署万象语法大模型
     home.file.".local/share/fcitx5/rime/wanxiang-lts-zh-hans.gram".source = builtins.fetchurl {
       url = cfg.extraGramUrl;
-      sha256 = "sha256-WnIdOzF9FYyzVs+waD+DHKvAaAI8EQTIYwMD4I9jhVE="
+      sha256 = "sha256-WnIdOzF9FYyzVs+waD+DHKvAaAI8EQTIYwMD4I9jhVE=";
     };
 
     # 默认启用雾凇 + 万象两个方案
