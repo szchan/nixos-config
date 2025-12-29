410c01a3741d5605221fc80a79e8b5b41ca351e3
szchan <mail.szchan@gmail.com>
2025-12-29 17:47:50 +0800

    refactor desktopEnvironment

--- home/szchan/desktopEnvironment/default.nix
+++ home/szchan/desktopEnvironment/default.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {   
-    imports = [
-        # ./niri/niri.nix
-        # ./shell/dms.nix
-        ./gnome/gnome.nix
-    ];
+  imports = [
+    # ./niri/niri.nix
+    # ./shell/dms.nix
+    ./gnome/gnome.nix
+  ];
 }
--- home/szchan/desktopEnvironment/shell/dms.nix
+++ home/szchan/desktopEnvironment/shell/dms.nix
@@ -2,32 +2,32 @@
 
 {   
 
-    programs.dankMaterialShell = {
-        enable = true;
-        # niri = {
-        #     enableKeybinds = true;   # Sets static preset keybinds
-        #     enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
-        # };
+	programs.dankMaterialShell = {
+		enable = true;
+		niri = {
+			enableKeybinds = true;   # Sets static preset keybinds
+			enableSpawn = true;      # Auto-start DMS with niri and cliphist, if enabled
+		};
 
-        enableSystemMonitoring = true;     # System monitoring widgets (dgop)
-        enableClipboard = true;            # Clipboard history manager
-        enableVPN = true;                  # VPN management widget
-        enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
-        enableAudioWavelength = true;      # Audio visualizer (cava)
-        enableCalendarEvents = true;       # Calendar integration (khal)
+		enableSystemMonitoring = true;     # System monitoring widgets (dgop)
+		enableClipboard = true;            # Clipboard history manager
+		enableVPN = true;                  # VPN management widget
+		enableDynamicTheming = true;       # Wallpaper-based theming (matugen)
+		enableAudioWavelength = true;      # Audio visualizer (cava)
+		enableCalendarEvents = true;       # Calendar integration (khal)
 
-        default.settings = {
-            theme = "dark";
-            dynamicTheming = true;
-            # Add any other settings here
-        };
+		default.settings = {
+			theme = "dark";
+			dynamicTheming = true;
+			# Add any other settings here
+		};
 
-        default.session = {
-            # Session state defaults
-        };
-    };
+		default.session = {
+			# Session state defaults
+		};
+	};
 
-    # systemd.user.services.niri-flake-polkit = {
-    #     enable = false;  # 或直接 lib.mkForce false;
-    # };
+	systemd.user.services.niri-flake-polkit = {
+		enable = false;  # 或直接 lib.mkForce false;
+	};
 }
