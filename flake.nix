b687332873afeffc9115b7db57dee892097fac12
szchan <mail.szchan@gmail.com>
2025-12-30 16:04:26 +0800

    add home-manager backup extention

--- flake.nix
+++ flake.nix
@@ -114,6 +114,7 @@
         home-manager.nixosModules.home-manager{
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "bak";
           home-manager.users.szchan = ./home/szchan/home.nix;
           home-manager.extraSpecialArgs = {
             inherit inputs;
