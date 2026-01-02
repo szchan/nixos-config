d112975147eff7aff8e90a6fe402ae00d76153db
szchan <mail.szchan@gmail.com>
2026-01-02 21:33:14 +0800

    add vivaldi and chrome extentions

--- home/szchan/internet/browser.nix
+++ home/szchan/internet/browser.nix
@@ -4,13 +4,17 @@
   programs.chromium = {
     enable = true;
     package = pkgs.google-chrome;
-    #extensions = [
-    #  { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
-    #  { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
-    #  { id = "fjkmabmdepjfammlpliljpnbhleegehm"; } # WebRTC Control
-    #  { id = "bdiifdefkgmcblbcghdlonllpjhhjgof"; } # KISS Translator
-    #  { id = "kefjpfngnndepjbopdmoebkipbgkggaa"; } # RSSHub Radar
-    #  { id = "jfgfiigpkhlkbnfnbobbkinehhfdhndo"; } # Page Assistss
-    #];
+    extensions = [
+      { id = "aeblfdkhhhdcdjpifhhbdiojplfjncoa"; } # 1Password
+      { id = "eimadpbcbfnmbkopoojfekhnkhdbieeh"; } # Dark Reader
+      { id = "fjkmabmdepjfammlpliljpnbhleegehm"; } # WebRTC Control
+      { id = "bdiifdefkgmcblbcghdlonllpjhhjgof"; } # KISS Translator
+      { id = "kefjpfngnndepjbopdmoebkipbgkggaa"; } # RSSHub Radar
+      { id = "jfgfiigpkhlkbnfnbobbkinehhfdhndo"; } # Page Assistss
+    ];
   };
+
+  home.packages = with pkgs; [
+    vivaldi
+  ];
 }
