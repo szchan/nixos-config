352f564b3349de289bd58442dec4d6bf90e731dd
szchan <mail.szchan@gmail.com>
2026-01-02 20:19:45 +0800

    update

--- configuration.nix
+++ configuration.nix
@@ -7,8 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      # ./host/vm.nix
-      ./hardware-configuration.nix
+      ./host/pc.nix
       
       # Include base configuration
       ./base
