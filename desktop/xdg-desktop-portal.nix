857cd88188bc1bb101fac24eccb225cf84a4fe07
szchan <mail.szchan@gmail.com>
2026-01-02 23:29:52 +0800

    config portal file chooser

--- desktop/xdg-desktop-portal.nix
+++ desktop/xdg-desktop-portal.nix
@@ -5,8 +5,31 @@
   xdg.portal = {
     enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+    config = {
+      # 文件选择器支持
+      common = {
+        default = [
+          "gtk"
+        ];
+      };
+      pantheon = {
+        default = [
+          "pantheon"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Secret" = [
+          "gnome-keyring"
+        ];
+      };
+      x-cinnamon = {
+        default = [
+          "xapp"
+          "gtk"
+        ];
+      };
+    }
   };
-  
+
   # 解决在WM中的portal相关bug
   environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
 }
