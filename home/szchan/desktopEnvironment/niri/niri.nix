0d3b9064de70f7cffc2c036304035a6d6fe8f71d
szchan <mail.szchan@gmail.com>
2026-01-02 23:19:43 +0800

    try stable version

--- home/szchan/desktopEnvironment/niri/niri.nix
+++ home/szchan/desktopEnvironment/niri/niri.nix
@@ -10,7 +10,7 @@
     enable = true;
 
     # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
-    package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
+    package = pkgs.niri-stable;  # 如果想用 stable，改成 pkgs.niri-stable
 
   };
 
