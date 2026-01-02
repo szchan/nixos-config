1ee9739886f07a230046b3bb201cc3aca4db28bf
szchan <mail.szchan@gmail.com>
2026-01-02 23:10:02 +0800

    fix syntax

--- desktop/xdg-desktop-portal.nix
+++ desktop/xdg-desktop-portal.nix
@@ -2,5 +2,5 @@
 
 {
   # 解决在WM中的portal相关bug
-  environment.pathsToLink = [ `/share/applications` `/share/xdg-desktop-portal` ];
+  environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
 }
