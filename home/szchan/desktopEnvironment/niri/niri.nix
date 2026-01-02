f2cfeb629fc3d4273d99bf023f663c421bbfd366
szchan <mail.szchan@gmail.com>
2026-01-03 00:00:54 +0800

    use niri-unstable

--- home/szchan/desktopEnvironment/niri/niri.nix
+++ home/szchan/desktopEnvironment/niri/niri.nix
@@ -10,9 +10,13 @@
     enable = true;
 
     # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
-    # package = pkgs.niri-stable;  # 如果想用 stable，改成 pkgs.niri-stable
+    package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
 
   };
+
+  home.packages = with pkgs; [
+    xwayland-satellite
+  ];
 
   # 显式启用dbus
   # services.dbus.enable = true;
