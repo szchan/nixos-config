75d16781d40869fa800aaa070bdf7dd7d2320965
szchan <mail.szchan@gmail.com>
2026-01-02 23:18:02 +0800

    try force off niri-flake-polkit

--- home/szchan/desktopEnvironment/niri/niri.nix
+++ home/szchan/desktopEnvironment/niri/niri.nix
@@ -6,13 +6,13 @@
     inputs.niri-flake.homeModules.niri
     ../shell/dms.nix
   ];
-  # programs.niri = {
-  #   enable = true;
+  programs.niri = {
+    enable = true;
 
-  #   # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
-  #   package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
+    # 推荐使用 unstable 版本（与当前 nixpkgs 的 mesa 等依赖匹配，避免黑屏）
+    package = pkgs.niri-unstable;  # 如果想用 stable，改成 pkgs.niri-stable
 
-  # };
+  };
 
   # 显式启用dbus
   # services.dbus.enable = true;
@@ -24,7 +24,8 @@
   };
 
   systemd.user.services.niri-flake-polkit = {
-		enable = false;  # 或直接 lib.mkForce false;
+		# enable = false;  # 或直接 lib.mkForce false;
+    lib.mkForce false;
 	};
 
 }
