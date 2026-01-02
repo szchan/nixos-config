113fe0b07030ca975cb816d1ae25d31e0a7ce24a
szchan <mail.szchan@gmail.com>
2026-01-02 23:11:39 +0800

    add xdg portal to global

--- desktop/xdg-desktop-portal.nix
+++ desktop/xdg-desktop-portal.nix
@@ -1,6 +1,12 @@
 {config, pkgs, ... }:
 
 {
+  # 启用portal（远程桌面支持）
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  };
+  
   # 解决在WM中的portal相关bug
   environment.pathsToLink = [ "/share/applications" "/share/xdg-desktop-portal" ];
 }
