91e6a9b71c527491b763d1df6fc18b1627c44338
szchan <mail.szchan@gmail.com>
2026-01-03 10:56:50 +0800

    use dms flake

--- desktop/niri/niri.nix
+++ desktop/niri/niri.nix
@@ -44,21 +44,21 @@
   };
 
   # Desktop Shell
-  programs.dms-shell = {
-    enable = true;
-    package = pkgs.dms-shell;
-    systemd = {
-      # enable = true;
-      target = "graphical-session.target";
-      restartIfChanged = true;
-    };
-    quickshell.package = pkgs.quickshell;
-    enableVPN = true;
-    enableSystemMonitoring = true;
-    enableDynamicTheming = true;
-    enableClipboard = true;
-    enableCalendarEvents = true;
-    enableAudioWavelength = true;
-  };
+  # programs.dms-shell = {
+  #   enable = true;
+  #   package = pkgs.dms-shell;
+  #   systemd = {
+  #     # enable = true;
+  #     target = "graphical-session.target";
+  #     restartIfChanged = true;
+  #   };
+  #   quickshell.package = pkgs.quickshell;
+  #   enableVPN = true;
+  #   enableSystemMonitoring = true;
+  #   enableDynamicTheming = true;
+  #   enableClipboard = true;
+  #   enableCalendarEvents = true;
+  #   enableAudioWavelength = true;
+  # };
 
 }
