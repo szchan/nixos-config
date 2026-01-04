07f26cc3311b33de111701ef2d4c6730b041508e
szchan <mail.szchan@gmail.com>
2026-01-04 14:49:07 +0800

    do not manage dms dotfiles

--- home/szchan/desktopEnvironment/dms-niri/niri-dotfiles.nix
+++ home/szchan/desktopEnvironment/dms-niri/niri-dotfiles.nix
@@ -2,9 +2,16 @@
 
 {
   xdg.configFile = {
-    "niri" = {
-      recursive = true;
-      source = ./niri-dotfiles;
-    };
+
+    "niri/config.kdl".source = ./niri-dotfiles/config.kdl;
+    
+    "niri/env.kdl".source = ./niri-dotfiles/env.kdl;
+
+    "niri/startup.kdl".source = ./niri-dotfiles/startup.kdl
+
+    # "niri" = {
+    #   recursive = true;
+    #   source = ./niri-dotfiles;
+    # };
   };
 }
