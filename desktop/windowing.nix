dc9b13aa95c370d8fc2a8b15b36f0f99a6d8dec6
szchan <mail.szchan@gmail.com>
2026-01-04 21:48:07 +0800

    test enable both x11 and wayland

--- desktop/windowing.nix
+++ desktop/windowing.nix
@@ -2,7 +2,7 @@
 
 {
   # 显式配置使用Wayland
-  services.xserver.enable = false;
+  services.xserver.enable = true;
     
   # 解决使用WM时 解决fcitx5无法启动
   services.xserver.desktopManager.runXdgAutostartIfNone = true;
