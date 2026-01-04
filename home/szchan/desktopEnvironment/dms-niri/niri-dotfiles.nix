4c96dc39db15d9c5f568d1d6ead7cca63f5132de
szchan <mail.szchan@gmail.com>
2026-01-04 17:00:26 +0800

    enable keybinding

--- home/szchan/desktopEnvironment/dms-niri/niri-dotfiles.nix
+++ home/szchan/desktopEnvironment/dms-niri/niri-dotfiles.nix
@@ -3,11 +3,17 @@
 {
   xdg.configFile = {
 
+    # 管理通用配置
     "niri/config.kdl".source = ./niri-dotfiles/config.kdl;
     
+    # 管理环境变量配置
     "niri/env.kdl".source = ./niri-dotfiles/env.kdl;
 
+    # 管理自启动配置
     "niri/startup.kdl".source = ./niri-dotfiles/startup.kdl;
+
+    # 管理快捷键配置
+    "niri/keybinding.kdl".source = ./niri-dotfiles/keybinding.kdl
 
     # "niri" = {
     #   recursive = true;
