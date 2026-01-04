52b40606bb2ab37240ba18dbb0ccb0b86b68dcb1
szchan <mail.szchan@gmail.com>
2026-01-04 21:55:23 +0800

    refactor cursor theme

--- home/szchan/desktopEnvironment/dms-niri/cursorTheme.nix
+++ home/szchan/desktopEnvironment/dms-niri/cursorTheme.nix
@@ -1,13 +1,16 @@
-{config, pkgs, ...}:
+{ config, pkgs, ... }:
 
 {
   gtk = {
-    enable = true;  # 必须启用
+    enable = true;
+  };
 
-    cursorTheme = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
-    };
+  home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;  # 可选，推荐设置大小，尤其是 HiDPI 屏幕，常见值 24/32/48
+    gtk.enable = true;  # 确保 GTK 应用也生效
+    # 如果你是 X11 会话，可以加 x11.enable = true; Wayland 下通常不需要
   };
 
   home.packages = [ pkgs.bibata-cursors ];
