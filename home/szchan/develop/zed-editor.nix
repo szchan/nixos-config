47dd56b10ee79f95e7c6c57747f5d92a62734f46
szchan <mail.szchan@gmail.com>
2026-01-05 20:19:30 +0800

    test zed

--- home/szchan/develop/zed-editor.nix
+++ home/szchan/develop/zed-editor.nix
@@ -3,7 +3,8 @@
 {
   programs.zed-editor = {
     enable = true;
-    package = inputs.zed.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    # package = inputs.zed.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    package = pkgs.zed-editor-fhs;
     extensions = [
       "nix"
     ];
