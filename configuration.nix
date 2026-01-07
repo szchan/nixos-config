c2ab43ae3520eecef2a9b2bb1a47c7300fa942fe
szchan <mail.szchan@gmail.com>
2026-01-07 09:33:02 +0800

    add user szchan to podman group

--- configuration.nix
+++ configuration.nix
@@ -37,7 +37,7 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.szchan = {
     isNormalUser = true;
-    extraGroups = [ "wheel" "libvirt" "kvm" "docker" "wireshark" "gamemode" ];
+    extraGroups = [ "wheel" "libvirt" "kvm" "docker" "podman" "wireshark" "gamemode" ];
     shell = pkgs.zsh;
     packages = with pkgs; [
     ];
