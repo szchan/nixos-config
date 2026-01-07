62857775a94e1d18cc569ec0e461bd70b56ce13d
szchan <mail.szchan@gmail.com>
2026-01-08 00:48:44 +0800

    fmt

--- home/szchan/virtualisation/distrobox.nix
+++ home/szchan/virtualisation/distrobox.nix
@@ -1,10 +1,9 @@
-{ config, pkgs, ... }:
+{ config, ... }:
 
 {
   programs.distrobox = {
     enable = true;
 
-    
     settings = {
       container_manager = "podman";
       container_user_custom_home = "${config.home.homeDirectory}/Data/distrobox";
